52759249f85baaee3dc6929758cc2d1c4d6f3148
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-21 21:29:21 +0300

    - Added onNewSocketMessage

--- app/src/main/java/memorizer/freecoders/com/flashcards/network/ServerInterface.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/network/ServerInterface.java
@@ -1,7 +1,35 @@
 package memorizer.freecoders.com.flashcards.network;
 
+import com.github.nkzawa.emitter.Emitter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import memorizer.freecoders.com.flashcards.common.MemorizerApplication;
+
 /**
  * Created by alex-mac on 21.11.15.
  */
 public class ServerInterface {
+
+    public Emitter.Listener onNewSocketMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object[] args) {
+            MemorizerApplication.getFlashCardActivity().
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                    } catch (JSONException e) {
+                        return;
+                    }
+                }
+            });
+        }
+    };
 }
