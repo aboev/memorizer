aa70c5aa4e7ab6c436fb7c379bd9dfc8f1a3f22e
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-21 20:41:14 +0300

    - Constants initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/common/Constants.java
@@ -0,0 +1,12 @@
+package memorizer.freecoders.com.flashcards.common;
+
+/**
+ * Created by alex-mac on 21.11.15.
+ */
+public class Constants {
+    public final static String LOG_TAG = "com.freecoders.flashcards";
+    public static final String PREFS_NAME = "MulticardsPrefs";
+
+    public final static String SERVER_URL = "http://dev.snufan.com:81";
+    public final static String SOCKET_SERVER_URL = "http://dev.snufan.com:5001";
+}
