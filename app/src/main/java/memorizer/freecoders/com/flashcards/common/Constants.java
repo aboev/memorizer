c1868a17da17e27c202cc0057d6fde554ce737c4
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-27 23:04:24 +0300

    - Added constants

--- app/src/main/java/memorizer/freecoders/com/flashcards/common/Constants.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/common/Constants.java
@@ -7,8 +7,21 @@ public class Constants {
     public final static String LOG_TAG = "com.freecoders.flashcards";
     public static final String PREFS_NAME = "MulticardsPrefs";
 
-    public final static String SERVER_URL = "http://dev.snufan.com:81";
-    public final static String SOCKET_SERVER_URL = "http://dev.snufan.com:5001";
+    public static final int STATUS_UNREGISTERED = 0;
+    public static final int STATUS_SMS_WAIT = 1;
+    public static final int STATUS_REGISTERED = 2;
+
+    public final static String SERVER_URL = "http://multicards.snufan.com:80";
+    public final static String SOCKET_SERVER_URL = "http://multicards.snufan.com:5001";
+    public final static String SERVER_PATH_USER = "/user";
+    public final static String SERVER_PATH_GAME = "/game";
+
+    public final static String RESPONSE_RESULT = "result";
+    public final static String RESPONSE_RESULT_OK = "OK";
+    public final static String RESPONSE_DATA = "data";
+    public final static String RESPONSE_CODE = "code";
 
     public final static String SOCKET_CHANNEL_NAME = "event";
+
+
 }
