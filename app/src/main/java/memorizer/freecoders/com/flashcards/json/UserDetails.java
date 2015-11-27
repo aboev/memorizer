f874fe992f090c55b702184c65d95988d2e3c88d
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-27 23:31:18 +0300

    - UserDetails initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/UserDetails.java
@@ -0,0 +1,23 @@
+package memorizer.freecoders.com.flashcards.json;
+
+/**
+ * Created by alex-mac on 27.11.15.
+ */
+public class UserDetails {
+
+    public String name = "";
+    public String phone = "";
+    public String email = "";
+    public String avatar = "";
+    public String id = "";
+    public String socket_id = "";
+    public Integer status;
+
+    public void setNullFields(){
+        if (name.isEmpty()) name = null;
+        if (phone.isEmpty()) phone = null;
+        if (email.isEmpty()) email = null;
+        if (avatar.isEmpty()) avatar = null;
+        if (id.isEmpty()) id = null;
+    }
+}
