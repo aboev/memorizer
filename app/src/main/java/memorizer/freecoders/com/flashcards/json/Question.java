9ffee1aceeba66a12bf12c44ba888ce6254fb57c
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-29 00:16:17 +0300

    - Question initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/Question.java
@@ -0,0 +1,12 @@
+package memorizer.freecoders.com.flashcards.json;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alex-mac on 28.11.15.
+ */
+public class Question {
+    public String question;
+    public ArrayList<String> options;
+    public int answer_id;
+ }
