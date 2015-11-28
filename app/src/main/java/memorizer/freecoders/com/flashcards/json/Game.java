5ddb02acb433208b56827bea14102c231cee818b
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-29 00:16:04 +0300

    - Game initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/Game.java
@@ -0,0 +1,11 @@
+package memorizer.freecoders.com.flashcards.json;
+
+import java.util.HashMap;
+
+/**
+ * Created by alex-mac on 28.11.15.
+ */
+public class Game {
+    public HashMap<String, String> players;
+    public Integer status;
+}
