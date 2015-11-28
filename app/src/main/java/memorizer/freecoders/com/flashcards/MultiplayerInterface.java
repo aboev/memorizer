04d27c96e5004c792380de705fa2ad19778e1ab2
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-29 00:19:34 +0300

    - Implemented startGame

--- app/src/main/java/memorizer/freecoders/com/flashcards/MultiplayerInterface.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/MultiplayerInterface.java
@@ -1,9 +1,20 @@
 package memorizer.freecoders.com.flashcards;
 
+import android.app.ProgressDialog;
+
+import com.android.volley.Response;
+
+import memorizer.freecoders.com.flashcards.common.Constants;
+import memorizer.freecoders.com.flashcards.common.MemorizerApplication;
+import memorizer.freecoders.com.flashcards.json.Game;
+import memorizer.freecoders.com.flashcards.network.ServerInterface;
+
 /**
  * Created by alex-mac on 21.11.15.
  */
 public class MultiplayerInterface {
+    public ProgressDialog progressDialog;
+
     public static int EVENT_INCOMING_INVITATION = 0;
     public static int EVENT_INVITATION_ACCEPTED = 10;
     public static int EVENT_NEW_QUESTION = 20;
@@ -14,5 +25,23 @@ public class MultiplayerInterface {
 
     public void renderEvent (int intEventType, String strData) {
 
+    }
+
+    public void startGame () {
+        if ((MemorizerApplication.getPreferences().strUserID != null) &&
+                !MemorizerApplication.getPreferences().strUserID.isEmpty()) {
+            ServerInterface.newGameRequest(MemorizerApplication.getFlashCardActivity(),
+                    new Response.Listener<Game>() {
+                        @Override
+                        public void onResponse(Game response) {
+                            if (response.status == Constants.GAME_STATUS_SEARCHING_PLAYERS) {
+                                progressDialog = ProgressDialog.show(
+                                        MemorizerApplication.getFlashCardActivity(),
+                                        "",
+                                        "Searching for opponents", true);
+                            }
+                        }
+                    }, null);
+        }
     }
 }
