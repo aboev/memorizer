611ccd5d6fac09b180caa714ec5f8fed20c10d7e
Aleksey Boev <boev.aleksey@gmail.com>
2015-11-29 22:39:54 +0300

    - Implemented renderEvent and invokeEvent

--- app/src/main/java/memorizer/freecoders/com/flashcards/MultiplayerInterface.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/MultiplayerInterface.java
@@ -1,19 +1,33 @@
 package memorizer.freecoders.com.flashcards;
 
 import android.app.ProgressDialog;
+import android.util.Log;
 
 import com.android.volley.Response;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
 
 import memorizer.freecoders.com.flashcards.common.Constants;
 import memorizer.freecoders.com.flashcards.common.MemorizerApplication;
 import memorizer.freecoders.com.flashcards.json.Game;
+import memorizer.freecoders.com.flashcards.json.SocketMessage;
 import memorizer.freecoders.com.flashcards.network.ServerInterface;
 
 /**
  * Created by alex-mac on 21.11.15.
  */
 public class MultiplayerInterface {
+
+    private static String LOG_TAG = "MultiPlayerInterface";
+
     public ProgressDialog progressDialog;
+
+    public Game currentGame;
+
+    Gson gson = new Gson();
 
     public static int EVENT_INCOMING_INVITATION = 0;
     public static int EVENT_INVITATION_ACCEPTED = 10;
@@ -24,7 +38,35 @@ public class MultiplayerInterface {
     public static int EVENT_FINISH_SESSION = 60;
 
     public void renderEvent (int intEventType, String strData) {
+        /*
+            Visualize opponent events
+         */
 
+        Log.d(LOG_TAG, "Rendering multiplayer event " + intEventType);
+        if (intEventType == EVENT_USER_ANSWER) {
+            Integer intAnswerID = Integer.valueOf(strData);
+            MemorizerApplication.getFlashCardActivity().currentFlashCardFragment.
+                    answerHighlight(intAnswerID);
+        }
+    }
+
+    public void invokeEvent (int intEventType, String strData) {
+        /*
+            Send user events to opponent
+         */
+
+        if (currentGame == null) return;
+        if (intEventType == EVENT_USER_ANSWER) {
+            SocketMessage msg = new SocketMessage();
+            msg.msg_type = Constants.SOCK_MSG_TYPE_PLAYER_ANSWERED;
+            msg.msg_body = strData;
+            Set<String> players = currentGame.players.keySet();
+            players.remove(MemorizerApplication.getPreferences().strSocketID);
+            msg.id_to = new ArrayList(players);
+            Set<String> socketIDs = currentGame.players.keySet();
+            socketIDs.remove(MemorizerApplication.getPreferences().strSocketID);
+            MemorizerApplication.getServerInterface().getSocketIO().emit("message", gson.toJson(msg));
+        }
     }
 
     public void startGame () {
