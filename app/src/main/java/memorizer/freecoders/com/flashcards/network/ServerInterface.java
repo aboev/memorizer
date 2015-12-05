54d2f5f2dd455dae784d1744b6e00b86945437e0
Aleksey Boev <boev.aleksey@gmail.com>
2015-12-06 00:56:19 +0300

    - Implemented updateUserDetailsRequest - Implemented uploadImageRequest - Implemented handler for GAME_END event

--- app/src/main/java/memorizer/freecoders/com/flashcards/network/ServerInterface.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/network/ServerInterface.java
@@ -2,6 +2,7 @@ package memorizer.freecoders.com.flashcards.network;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -12,12 +13,13 @@ import com.google.gson.reflect.TypeToken;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import memorizer.freecoders.com.flashcards.MainMenuFragment;
+import memorizer.freecoders.com.flashcards.R;
 import memorizer.freecoders.com.flashcards.common.Constants;
 import memorizer.freecoders.com.flashcards.common.MemorizerApplication;
 import memorizer.freecoders.com.flashcards.json.Game;
@@ -38,7 +40,7 @@ public class ServerInterface {
     private Socket mSocketIO;
     private String strSocketID;
 
-    public static final void registerUserRequest(Context context,
+    public static final void registerUserRequest(
             UserDetails userDetails,
             final Response.Listener<String> responseListener,
             final Response.ErrorListener errorListener) {
@@ -75,10 +77,50 @@ public class ServerInterface {
             }
         }
         );
-        VolleySingleton.getInstance(context).addToRequestQueue(request);
+        VolleySingleton.getInstance(MemorizerApplication.getMainActivity()).
+                addToRequestQueue(request);
     }
 
-    public static final void newGameRequest(Context context,
+    public static final void updateUserDetailsRequest(
+             UserDetails userDetails,
+             final Response.Listener<String> responseListener,
+             final Response.ErrorListener errorListener) {
+        HashMap<String, String> headers = makeHTTPHeaders();
+        Log.d(LOG_TAG, "Update user details request");
+        StringRequest request = new StringRequest(Request.Method.PUT,
+                Constants.SERVER_URL + Constants.SERVER_PATH_USER ,
+                gson.toJson(userDetails), headers,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(LOG_TAG, "Response: " + response);
+                        Type type = new TypeToken<ServerResponse
+                                <String>>(){}.getType();
+                        try {
+                            ServerResponse<String> res =
+                                    gson.fromJson(response, type);
+                            if ( res != null && res.isSuccess() && responseListener != null)
+                                responseListener.onResponse(null);
+                            else if (errorListener != null)
+                                errorListener.onErrorResponse(new VolleyError());
+                        } catch (Exception e) {
+                            Log.d(LOG_TAG, "Exception: " + e.getLocalizedMessage());
+                            if (errorListener != null) errorListener.onErrorResponse(
+                                    new VolleyError());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (errorListener != null) errorListener.onErrorResponse(error);
+            }
+        }
+        );
+        VolleySingleton.getInstance(MemorizerApplication.getMainActivity()).
+                addToRequestQueue(request);
+    }
+
+    public static final void newGameRequest(
             final Response.Listener<Game> responseListener,
             final Response.ErrorListener errorListener) {
         HashMap<String, String> headers = makeHTTPHeaders();
@@ -113,14 +155,46 @@ public class ServerInterface {
             }
         }
         );
-        VolleySingleton.getInstance(context).addToRequestQueue(request);
+        VolleySingleton.getInstance(MemorizerApplication.getMainActivity()).
+                addToRequestQueue(request);
+    }
+
+    public void uploadImageRequest(final String strImageURI) {
+        HashMap<String, String> headers = makeHTTPHeaders();
+        MultipartRequest uploadRequest = new MultipartRequest(
+                Constants.SERVER_URL+Constants.SERVER_PATH_UPLOAD,
+                strImageURI,
+                headers,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(LOG_TAG, response.toString());
+                        try {
+                            MemorizerApplication.getPreferences().strAvatar = strImageURI;
+                            MemorizerApplication.getPreferences().savePreferences();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Log.d(LOG_TAG, "Exception " + e.getLocalizedMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.d(LOG_TAG, "Error: " + error.getMessage());
+            }
+        }
+        );
+        VolleySingleton.getInstance(MemorizerApplication.
+                getMainActivity()).addToRequestQueue(uploadRequest);
     }
 
     public void setSocketIO (Socket socket){
         this.mSocketIO = socket;
     }
 
-    public Socket getSocketIO () {
+    public Socket getSocketIO() {
         return this.mSocketIO;
     }
 
@@ -136,7 +210,7 @@ public class ServerInterface {
         @Override
         public void call(final Object[] args) {
             Log.d(LOG_TAG, "Received message " + args[0].toString());
-            MemorizerApplication.getFlashCardActivity().
+            MemorizerApplication.getMainActivity().
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -160,17 +234,15 @@ public class ServerInterface {
                                 && (MemorizerApplication.getMultiplayerInterface().progressDialog.
                                 isShowing()))
                             MemorizerApplication.getMultiplayerInterface().progressDialog.dismiss();
-                        Type type = new TypeToken<SocketMessage<Question>>() {
-                        }.getType();
+                        Type type = new TypeToken<SocketMessage<Question>>() {}.getType();
                         SocketMessage<Question> socketMessage = gson.fromJson(args[0].toString(), type);
-                        MemorizerApplication.getFlashCardActivity().nextFlashCard(socketMessage.msg_body);
-                    } else if (strMessageType.
-                            equals(Constants.SOCK_MSG_TYPE_GAME_START)) {
-                        Type type = new TypeToken<SocketMessage<Game>>() {
-                        }.getType();
+                        MemorizerApplication.getMainActivity().nextFlashCard(socketMessage.msg_body);
+                    } else if (strMessageType.equals(Constants.SOCK_MSG_TYPE_GAME_START)) {
+                        Type type = new TypeToken<SocketMessage<Game>>() {}.getType();
                         SocketMessage<Game> socketMessage = gson.fromJson(args[0].toString(), type);
                         MemorizerApplication.getMultiplayerInterface().currentGame =
                                 socketMessage.msg_body;
+                        MemorizerApplication.getMainActivity().showPlayersInfo();
                     } else if (strMessageType.
                             equals(Constants.SOCK_MSG_TYPE_PLAYER_ANSWERED)) {
                         Type type = new TypeToken<SocketMessage<String>>() {}.getType();
@@ -179,19 +251,21 @@ public class ServerInterface {
                         MemorizerApplication.getMultiplayerInterface().
                                 renderEvent(MemorizerApplication.getMultiplayerInterface().
                                         EVENT_USER_ANSWER, strAnswerID);
+                    } else if (strMessageType.
+                            equals(Constants.SOCK_MSG_TYPE_GAME_END)) {
+                        Toast.makeText(MemorizerApplication.getMainActivity(), "Game Over",
+                                Toast.LENGTH_LONG);
+                        MainMenuFragment mainMenuFragment = new MainMenuFragment();
+                        MemorizerApplication.getMainActivity().getFragmentManager()
+                                .beginTransaction().add(R.id.fragment_flashcard_container,
+                                mainMenuFragment).commit();
+                        MemorizerApplication.getMainActivity().getFragmentManager()
+                                .beginTransaction().remove(MemorizerApplication.getMainActivity().
+                                playersInfoFragment).commit();
+                        MemorizerApplication.getMainActivity().getFragmentManager()
+                                .beginTransaction().remove(MemorizerApplication.getMainActivity().
+                                currentFlashCardFragment).commit();
                     }
-/*
-                    try {
-                    JSONObject data = (JSONObject) args[0];
-                    String username;
-                    String message;
-                    try {
-                        username = data.getString("username");
-                        message = data.getString("message");
-                    } catch (JSONException e) {
-                        return;
-                    }
-*/
                 }
             });
         }
