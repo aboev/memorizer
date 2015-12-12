f5ec7bf5982bf3d8716e249ef547bf29e8811b35
Aleksey Boev <boev.aleksey@gmail.com>
2015-12-12 20:13:56 +0300

    - CardSet initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/CardSet.java
@@ -0,0 +1,10 @@
+package memorizer.freecoders.com.flashcards.json;
+
+/**
+ * Created by alex-mac on 12.12.15.
+ */
+public class CardSet {
+    String id;
+    String name;
+    String language;
+}
