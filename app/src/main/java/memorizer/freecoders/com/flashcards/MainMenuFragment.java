346fca01bce038a43fc3fe48d9d556bfff5ea285
Aleksey Boev <boev.aleksey@gmail.com>
2015-12-13 21:49:18 +0300

    - Changed main menu buttons

--- app/src/main/java/memorizer/freecoders/com/flashcards/MainMenuFragment.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/MainMenuFragment.java
@@ -1,13 +1,17 @@
 package memorizer.freecoders.com.flashcards;
 
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 
+import memorizer.freecoders.com.flashcards.common.Animations;
 import memorizer.freecoders.com.flashcards.common.Constants;
 import memorizer.freecoders.com.flashcards.common.MemorizerApplication;
 
@@ -19,18 +23,24 @@ public class MainMenuFragment extends Fragment {
     private Button buttonMultiplayer;
     private Button buttonSettings;
 
+    private TextView textViewTrain;
+    private TextView textViewMultiplayer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         setRetainInstance(true);
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.main_menu, container, false);
+        final View view = inflater.inflate(R.layout.main_menu, container, false);
 
         buttonTrain = (Button) view.findViewById(R.id.buttonTrain);
         buttonMultiplayer = (Button) view.findViewById(R.id.buttonMultiplayer);
         buttonSettings = (Button) view.findViewById(R.id.buttonSettings);
 
-        buttonTrain.setOnClickListener(new View.OnClickListener() {
+        textViewTrain = (TextView) view.findViewById(R.id.TextView_StyleButtonTrain);
+        textViewMultiplayer = (TextView) view.findViewById(R.id.TextView_StyleButtonMultiplayer);
+
+        textViewTrain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MemorizerApplication.getMainActivity().nextFlashCard();
@@ -45,15 +55,15 @@ public class MainMenuFragment extends Fragment {
             }
         });
 
-        buttonMultiplayer.setOnClickListener(new View.OnClickListener() {
+        textViewMultiplayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MultiplayerInterface multiplayerInterface = new MultiplayerInterface();
                 MemorizerApplication.setMultiPlayerInterface(multiplayerInterface);
                 multiplayerInterface.requestNewGame();
-                MemorizerApplication.getMainActivity().intUIState = Constants.UI_STATE_MULTIPLAYER_MODE;
             }
         });
+
 
         MemorizerApplication.getMainActivity().intUIState = Constants.UI_STATE_MAIN_MENU;
 
