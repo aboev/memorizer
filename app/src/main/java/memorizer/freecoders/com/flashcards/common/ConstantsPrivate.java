0d897e74f242f956f4b1233d1741d1511b423edf
Aleksey Boev <boev.aleksey@gmail.com>
2015-12-19 18:13:48 +0300

    - ConstantsPrivate initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/common/ConstantsPrivate.java
@@ -0,0 +1,8 @@
+package memorizer.freecoders.com.flashcards.common;
+
+/**
+ * Created by alex-mac on 13.12.15.
+ */
+public class ConstantsPrivate {
+    public static final String QUIZLET_CLIENT_ID = "";  // Refer to http://www.quizlet.com/api
+}
