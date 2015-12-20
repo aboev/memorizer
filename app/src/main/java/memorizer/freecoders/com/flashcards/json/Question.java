99657fad7840632dc2aebb9cca3f12f1cfde4cc3
Aleksey Boev <boev.aleksey@gmail.com>
2015-12-20 19:18:33 +0300

    - Added question_id

--- app/src/main/java/memorizer/freecoders/com/flashcards/json/Question.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/Question.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
  * Created by alex-mac on 28.11.15.
  */
 public class Question {
+    public int question_id;
     public String question;
     public ArrayList<String> options;
     public int answer_id;
