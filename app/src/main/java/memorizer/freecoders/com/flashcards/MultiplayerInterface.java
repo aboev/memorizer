8dc0069df4061d57364f7dd189a47472d12801bd
Aleksey Boev <boev.aleksey@gmail.com>
2015-12-27 22:19:33 +0300

    - Implemented GameData internal class - Move irrelevant logic to GameplayInterface

--- app/src/main/java/memorizer/freecoders/com/flashcards/MultiplayerInterface.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/MultiplayerInterface.java
@@ -16,6 +16,7 @@ import memorizer.freecoders.com.flashcards.json.Game;
 import memorizer.freecoders.com.flashcards.json.Question;
 import memorizer.freecoders.com.flashcards.json.SocketMessage;
 import memorizer.freecoders.com.flashcards.network.ServerInterface;
+import memorizer.freecoders.com.flashcards.network.SocketInterface;
 
 /**
  * Created by alex-mac on 21.11.15.
@@ -26,11 +27,8 @@ public class MultiplayerInterface {
 
     public ProgressDialog progressDialog;
 
-    public Game currentGame;
-    private Question currentQuestion;
+    public GameData currentGame;
     private int currentAnswer;
-    private String strUserStatus;
-    private Boolean boolAnswerConfirmed;
 
     Gson gson = new Gson();
 
@@ -44,113 +42,31 @@ public class MultiplayerInterface {
     public static int EVENT_FINISH_SESSION = 70;
     public static int EVENT_USER_WAIT = 80;     // User ready for next question
 
-    public void invokeEvent (int intEventType, String strData) {
-        /*
-            Send user events to opponent
-         */
-
+    public void invokeEvent (int intEventType, String strData) {    // Deliver events to server
         if (currentGame == null) return;
         if (intEventType == EVENT_USER_ANSWER) {    // User answered
-            SocketMessage msg = new SocketMessage();
-            msg.msg_type = Constants.SOCK_MSG_TYPE_PLAYER_ANSWERED;
-            msg.msg_body = strData;
-            msg.id_to = new ArrayList();
-            Multicards.getServerInterface().getSocketIO().emit("message", gson.toJson(msg));
+            SocketInterface.emitPlayerAnswered(strData);
             eventUserAnswer(Integer.valueOf(strData));
         } else if (intEventType == EVENT_USER_WAIT) {   // User ready for next question
-            SocketMessage msg = new SocketMessage();
-            msg.msg_type = Constants.SOCK_MSG_TYPE_PLAYER_STATUS_UPDATE;
-            msg.msg_body = Constants.PLAYER_STATUS_WAITING;
-            Multicards.getServerInterface().getSocketIO().emit("message", gson.toJson(msg));
+            SocketInterface.emitStatusUpdate(Constants.PLAYER_STATUS_WAITING);
         } else if (intEventType == EVENT_USER_THINK) {   // User ready for next question
-            SocketMessage msg = new SocketMessage();
-            msg.msg_type = Constants.SOCK_MSG_TYPE_PLAYER_STATUS_UPDATE;
-            msg.msg_body = Constants.PLAYER_STATUS_THINKING;
-            Multicards.getServerInterface().getSocketIO().emit("message", gson.toJson(msg));
+            SocketInterface.emitStatusUpdate(Constants.PLAYER_STATUS_THINKING);
         }
     }
 
     public void quitGame () {
-        SocketMessage msg = new SocketMessage();
-        msg.msg_type = Constants.SOCK_MSG_TYPE_QUIT_GAME;
-        msg.msg_body = "";
-        Multicards.getServerInterface().getSocketIO().emit("message", gson.toJson(msg));
-    }
-
-    public void requestNewGame(final String strGID) {
-        if ((Multicards.getPreferences().strUserID != null) &&
-                !Multicards.getPreferences().strUserID.isEmpty()) {
-
-            if ((Multicards.getPreferences().strUserName == null) ||
-                    Multicards.getPreferences().strUserName.isEmpty())
-                InputDialogInterface.updateUserName(new CallbackInterface() {
-                    @Override
-                    public void onResponse(Object obj) {
-                        ServerInterface.newGameRequest(strGID,
-                            new Response.Listener<Game>() {
-                                @Override
-                                public void onResponse(Game response) {
-                                    if (response.status == Constants.GAME_STATUS_SEARCHING_PLAYERS) {
-                                        progressDialog = ProgressDialog.show(
-                                                Multicards.getMainActivity(),
-                                                "",
-                                                "Searching for opponents", true);
-                                        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                            @Override
-                                            public void onCancel(DialogInterface dialog) {
-                                                quitGame();
-                                            }
-                                        });
-                                    }
-                                    progressDialog.setCancelable(true);
-                                }
-                            }, null);
-                    }
-                });
-            else
-                ServerInterface.newGameRequest(strGID,
-                        new Response.Listener<Game>() {
-                            @Override
-                            public void onResponse(Game response) {
-                                if (response.status == Constants.GAME_STATUS_SEARCHING_PLAYERS) {
-                                    progressDialog = ProgressDialog.show(
-                                            Multicards.getMainActivity(),
-                                            "",
-                                            "Searching for opponents", true);
-                                    progressDialog.setCancelable(true);
-                                    progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                        @Override
-                                        public void onCancel(DialogInterface dialog) {
-                                            quitGame();
-                                        }
-                                    });
-                                }
-                            }
-                }, null);
-        }
-    }
-
-    public void setCurrentQuestion(Question question) {
-        Question newQuestion = new Question();
-        newQuestion.question = question.question;
-        newQuestion.options = new ArrayList<String>();
-        newQuestion.options.addAll(question.options);
-        newQuestion.answer_id = question.answer_id;
-        newQuestion.question_id = question.question_id;
-        currentQuestion = newQuestion;
-    }
-
-    public Question getCurrentQuestion() {
-        return currentQuestion;
+        currentGame = null;
+        SocketInterface.emitQuitGame();
     }
 
     public void eventUserAnswer(int answerID) {
         currentAnswer = answerID;
-        strUserStatus = Constants.PLAYER_STATUS_ANSWERED;
+        currentGame.strUserStatus = Constants.PLAYER_STATUS_ANSWERED;
     }
 
     public void eventAnswerAccepted (int questionID) {
-        if ((currentQuestion != null) && (currentQuestion.question_id == questionID)) {
+        if ((currentGame.currentQuestion != null) &&
+                (currentGame.currentQuestion.question_id == questionID)) {
             Multicards.getMainActivity().currentFlashCardFragment.
                 answerHighlight(currentAnswer, false, null);
             CallbackInterface onAnimationEnd = new CallbackInterface() {
@@ -169,24 +85,25 @@ public class MultiplayerInterface {
                         onAnimationEnd);
             }
             Multicards.getMainActivity().playersInfoFragment.updateScore();
-            strUserStatus = Constants.PLAYER_STATUS_ANSWERED;
+            currentGame.strUserStatus = Constants.PLAYER_STATUS_ANSWERED;
         }
-        boolAnswerConfirmed = true;
+        currentGame.boolAnswerConfirmed = true;
     }
 
     public void eventAnswerRejected(int questionID) {
         invokeEvent(EVENT_USER_WAIT, "");
-        boolAnswerConfirmed = true;
+        currentGame.boolAnswerConfirmed = true;
     }
 
     public void eventOpponentAnswer(String strAnswerID) {
         Integer intAnswerID = Integer.valueOf(strAnswerID);
         Multicards.getMainActivity().currentFlashCardFragment.
             answerHighlight(intAnswerID, true, null);
-        if (Multicards.getMainActivity().currentFlashCardFragment.
-                mFlashCard.answer_id == intAnswerID) {
+        if (Multicards.getMainActivity().currentFlashCardFragment.mFlashCard.answer_id
+                == intAnswerID) {
             CallbackInterface onAnimationEnd = null;
-            if (boolAnswerConfirmed || (!strUserStatus.equals(Constants.PLAYER_STATUS_ANSWERED))) {
+            if (currentGame.boolAnswerConfirmed ||
+                    (!currentGame.strUserStatus.equals(Constants.PLAYER_STATUS_ANSWERED))) {
                 onAnimationEnd = new CallbackInterface() {
                     @Override
                     public void onResponse(Object obj) {
@@ -205,15 +122,43 @@ public class MultiplayerInterface {
     }
 
     public void eventNewQuestion (Question question) {
-        if ((progressDialog != null) && (progressDialog.isShowing()))
-            progressDialog.dismiss();
         Multicards.getMainActivity().nextFlashCard(question);
-        invokeEvent(Multicards.getMultiplayerInterface().EVENT_USER_THINK, "");
-        Multicards.getMainActivity().intUIState = Constants.UI_STATE_MULTIPLAYER_MODE;
-        setCurrentQuestion(question);
-        strUserStatus = Constants.PLAYER_STATUS_THINKING;
-        boolAnswerConfirmed = false;
+
+        SocketInterface.emitStatusUpdate(Constants.PLAYER_STATUS_THINKING);
+
+        if (currentGame == null)
+            currentGame = new GameData();
+        currentGame.setCurrentQuestion(question);
+        currentGame.strUserStatus = Constants.PLAYER_STATUS_THINKING;
+        currentGame.boolAnswerConfirmed = false;
         Multicards.getMainActivity().playersInfoFragment.updateScore();
+    }
+
+    public void setGameData(Game game, String strGID) {
+        if (currentGame == null)
+            currentGame = new GameData();
+        if (game != null)
+            currentGame.game = game;
+        if (strGID != null)
+            currentGame.strGID = strGID;
+    }
+
+    public class GameData {
+        public Game game;
+        public String strGID = "";
+        public String strUserStatus = "";
+        public Question currentQuestion;
+        public Boolean boolAnswerConfirmed = false;
+
+        public void setCurrentQuestion (Question question) {
+            Question newQuestion = new Question();
+            newQuestion.question = question.question;
+            newQuestion.options = new ArrayList<String>();
+            newQuestion.options.addAll(question.options);
+            newQuestion.answer_id = question.answer_id;
+            newQuestion.question_id = question.question_id;
+            currentQuestion = newQuestion;
+        }
     }
 
 }
