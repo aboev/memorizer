448a3943d31fc5bd1e95de995f7e9307be2bfe0f
Aleksey Boev <boev.aleksey@gmail.com>
2016-01-11 21:59:26 +0300

    - SocketMessageExtra initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/SocketMessageExtra.java
@@ -0,0 +1,13 @@
+package memorizer.freecoders.com.flashcards.json;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alex-mac on 06.01.16.
+ */
+public class SocketMessageExtra<T, K> {
+    public ArrayList<String> id_to = new ArrayList<>();
+    public String msg_type = "";
+    public T msg_body;
+    public K msg_extra;
+}
