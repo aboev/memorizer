fadd50ba59f1d44fb1e879e7b556c79b60dd45bc
Aleksey Boev <boev.aleksey@gmail.com>
2016-01-12 19:24:58 +0300

    - Added host/join game options

--- app/src/main/java/memorizer/freecoders/com/flashcards/fragments/MainMenuFragment.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/fragments/MainMenuFragment.java
@@ -1,6 +1,7 @@
 package memorizer.freecoders.com.flashcards.fragments;
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,9 +10,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import memorizer.freecoders.com.flashcards.CardsetPickerActivity;
+import memorizer.freecoders.com.flashcards.GameplayManager;
 import memorizer.freecoders.com.flashcards.R;
+import memorizer.freecoders.com.flashcards.classes.CallbackInterface;
 import memorizer.freecoders.com.flashcards.common.Constants;
+import memorizer.freecoders.com.flashcards.common.InputDialogInterface;
 import memorizer.freecoders.com.flashcards.common.Multicards;
+import memorizer.freecoders.com.flashcards.network.ServerInterface;
 
 /**
  * Created by alex-mac on 22.11.15.
@@ -44,11 +49,38 @@ public class MainMenuFragment extends Fragment {
         textViewMultiplayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Multicards.getMainActivity(),
-                        CardsetPickerActivity.class);
-                intent.putExtra(Constants.INTENT_META_NEXT_FRAGMENT,
-                        Constants.UI_STATE_MULTIPLAYER_MODE);
-                startActivity(intent);
+                InputDialogInterface.showPickOpponentDialog(new CallbackInterface() {
+                    @Override
+                    public void onResponse(Object obj) {
+                        Integer index = (Integer) obj;
+                        if (index == 0) {
+                            Intent intent = new Intent(Multicards.getMainActivity(),
+                                    CardsetPickerActivity.class);
+                            intent.putExtra(Constants.INTENT_META_NEXT_FRAGMENT,
+                                    Constants.UI_STATE_MULTIPLAYER_MODE);
+                            startActivity(intent);
+                        } else if (index == 1) {
+                            GameplayManager.strOpponentName = "-1";
+                            Intent intent = new Intent(Multicards.getMainActivity(),
+                                    CardsetPickerActivity.class);
+                            intent.putExtra(Constants.INTENT_META_NEXT_FRAGMENT,
+                                    Constants.UI_STATE_MULTIPLAYER_MODE);
+                            startActivity(intent);
+                        } else if (index == 2) {
+                            InputDialogInterface.showEnterOpponentNameDialog(new CallbackInterface() {
+                                @Override
+                                public void onResponse(Object obj) {
+                                    if (obj != null) {
+                                        String strOpponentName = (String) obj;
+                                        GameplayManager.strOpponentName = strOpponentName;
+                                        ServerInterface.newGameRequest("", strOpponentName,
+                                                null, null);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
             }
         });
 
