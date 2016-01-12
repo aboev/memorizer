6f2cb2ab5ae651a497f6b6af384ac8f5cde6ea5e
Aleksey Boev <boev.aleksey@gmail.com>
2016-01-12 16:56:45 +0300

    - Added gid, title

--- app/src/main/java/memorizer/freecoders/com/flashcards/json/CardSet.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/CardSet.java
@@ -4,9 +4,11 @@ package memorizer.freecoders.com.flashcards.json;
  * Created by alex-mac on 12.12.15.
  */
 public class CardSet {
-    String id;
-    String name;
-    String language;
+    public String gid;
+    String cardset_id;
+    public String title;
+    String lang_terms;
+    String lang_definitions;
     String[] likes = new String[]{};
     Integer like_count;
 }
