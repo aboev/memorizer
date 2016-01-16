4101a2a5667f9c140ad40a0800b320ce85f6a520
Aleksey Boev <boev.aleksey@gmail.com>
2016-01-17 00:12:13 +0300

    - Implemented getName

--- app/src/main/java/memorizer/freecoders/com/flashcards/json/TagDescriptor.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/TagDescriptor.java
@@ -1,7 +1,26 @@
 package memorizer.freecoders.com.flashcards.json;
 
+import java.util.HashMap;
+
+import memorizer.freecoders.com.flashcards.common.Constants;
+import memorizer.freecoders.com.flashcards.utils.Utils;
+
 /**
  * Created by alex-mac on 16.01.16.
  */
 public class TagDescriptor {
+    public String tag_id;
+    public HashMap<String, String> tag_name;
+    public String color;
+
+    public String getName () {
+        String strRes = "";
+        if (tag_name != null) {
+            if (tag_name.containsKey(Utils.getLocale())) strRes =
+                    tag_name.get(Utils.getLocale());
+            else if (tag_name.containsKey(Constants.DEFAULT_LOCALE)) strRes =
+                    tag_name.get(Constants.DEFAULT_LOCALE);
+        }
+        return strRes;
+    }
 }
