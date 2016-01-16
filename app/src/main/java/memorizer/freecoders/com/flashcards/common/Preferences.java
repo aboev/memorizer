88d572936e913e168f6c9587a10e33519300caa2
Aleksey Boev <boev.aleksey@gmail.com>
2016-01-17 00:11:11 +0300

    - Added KEY_TAG_DESCRIPTORS, KEY_RECENT_SET_DESCRIPTORS

--- app/src/main/java/memorizer/freecoders/com/flashcards/common/Preferences.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/common/Preferences.java
@@ -2,6 +2,7 @@ package memorizer.freecoders.com.flashcards.common;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -9,7 +10,10 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 
+import memorizer.freecoders.com.flashcards.json.CardSet;
+import memorizer.freecoders.com.flashcards.json.TagDescriptor;
 import memorizer.freecoders.com.flashcards.json.UserDetails;
+import memorizer.freecoders.com.flashcards.json.quizlet.QuizletCardsetDescriptor;
 
 /**
  * Created by alex-mac on 27.11.15.
@@ -29,7 +33,9 @@ public final class Preferences {
     private String KEY_SOCKET_ID = "socket_id";
     private String KEY_AVATAR = "avatar";
     private String KEY_RECENT_SETS = "recent_sets";
+    private String KEY_RECENT_SET_DESCRIPTORS = "recent_set_descriptors";
     private String KEY_RECENT_OPPONENTS = "recent_opponents";
+    private String KEY_TAG_DESCRIPTORS = "tag_descriptors";
 
     public String strUserID = "";
     public String strUserName = "";
@@ -42,7 +48,9 @@ public final class Preferences {
     public Integer intLastOpenedTab = 0;
     public Boolean boolFirstStart = true;
     public HashMap<String, Long> recentSets;
+    public HashMap<String, QuizletCardsetDescriptor> recentSetDescriptors;
     public HashMap<UserDetails, Long> recentOpponents;
+    public HashMap<String, TagDescriptor> tagDescriptors;
 
     public Preferences(Context context) {
         settings = context.getSharedPreferences(Constants.PREFS_NAME, 0);
@@ -61,8 +69,14 @@ public final class Preferences {
         Type type = new TypeToken<HashMap<String, Long>>() {}.getType();
         recentSets = gson.fromJson(settings.getString(KEY_RECENT_SETS, "{}"), type);
 
+        type = new TypeToken<HashMap<String, QuizletCardsetDescriptor>>() {}.getType();
+        recentSetDescriptors = gson.fromJson(settings.getString(KEY_RECENT_SET_DESCRIPTORS, "{}"), type);
+
         type = new TypeToken<HashMap<UserDetails, Long>>() {}.getType();
         recentOpponents = gson.fromJson(settings.getString(KEY_RECENT_OPPONENTS, "{}"), type);
+
+        type = new TypeToken<HashMap<String, TagDescriptor>>() {}.getType();
+        tagDescriptors = gson.fromJson(settings.getString(KEY_TAG_DESCRIPTORS, "{}"), type);
 
         return ((strUserID.length() != 0) && (intRegisterStatus == Constants.STATUS_REGISTERED));
     }
@@ -77,7 +91,9 @@ public final class Preferences {
         editor.putInt(KEY_REGISTER_STATUS, intRegisterStatus);
         editor.putBoolean(KEY_FIRST_START, boolFirstStart);
         editor.putString(KEY_RECENT_SETS, gson.toJson(recentSets));
+        editor.putString(KEY_RECENT_SET_DESCRIPTORS, gson.toJson(recentSetDescriptors));
         editor.putString(KEY_RECENT_OPPONENTS, gson.toJson(recentOpponents));
+        editor.putString(KEY_TAG_DESCRIPTORS, gson.toJson(tagDescriptors));
         editor.commit();
     }
 
