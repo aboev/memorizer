4e2993d746d9d721620a294d019c7f7e9ba7e8c5
Aleksey Boev <boev.aleksey@gmail.com>
2016-01-17 00:09:41 +0300

    - Implemented TagView

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/classes/TagView.java
@@ -0,0 +1,105 @@
+package memorizer.freecoders.com.flashcards.classes;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import memorizer.freecoders.com.flashcards.R;
+import memorizer.freecoders.com.flashcards.common.Multicards;
+import memorizer.freecoders.com.flashcards.json.TagDescriptor;
+
+/**
+ * Created by alex-mac on 16.01.16.
+ */
+public class TagView extends FrameLayout {
+
+    private TextView textViewTagName;
+    private String strTagID = "";
+    private int color;
+    private Boolean boolSelected = false;
+    private Drawable background;
+    private View view;
+
+    public TagView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView();
+    }
+
+    public TagView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public TagView(Context context) {
+        super(context);
+        initView();
+    }
+
+    private void initView() {
+        view = inflate(getContext(), R.layout.tag_view, null);
+        textViewTagName = (TextView) view.findViewById(R.id.textViewTagName);
+        background = textViewTagName.getBackground();
+        addView(view);
+    }
+
+    public String getName() {
+        return textViewTagName.getText().toString();
+    }
+
+    public String getTagID() {
+        return this.strTagID;
+    }
+
+    public void setTag (TagDescriptor tag) {
+        textViewTagName.setText(tag.getName());
+        this.strTagID = tag.tag_id;
+        if ((tag.color != null) && (!tag.color.isEmpty())) {
+            color = Color.parseColor(tag.color);
+            if (background instanceof ShapeDrawable) {
+                ShapeDrawable shapeDrawable = (ShapeDrawable)background;
+                shapeDrawable.getPaint().setColor(color);
+            } else if (background instanceof GradientDrawable) {
+                GradientDrawable gradientDrawable = (GradientDrawable)background;
+                gradientDrawable.setColor(color);
+            }
+        }
+        invalidate();
+    }
+
+    public Boolean onSelect () {
+        if (this.boolSelected)
+            this.boolSelected = false;
+        else
+            this.boolSelected = true;
+        if (boolSelected) {
+            int colorSelected =
+                    ContextCompat.getColor(Multicards.getMainActivity(), R.color.colorSelected);
+            if (background instanceof ShapeDrawable) {
+                ShapeDrawable shapeDrawable = (ShapeDrawable)background;
+                shapeDrawable.getPaint().setColor(colorSelected);
+            } else if (background instanceof GradientDrawable) {
+                GradientDrawable gradientDrawable = (GradientDrawable)background;
+                gradientDrawable.setColor(colorSelected);
+            }
+        } else {
+            if (background instanceof ShapeDrawable) {
+                ShapeDrawable shapeDrawable = (ShapeDrawable)background;
+                shapeDrawable.getPaint().setColor(color);
+            } else if (background instanceof GradientDrawable) {
+                GradientDrawable gradientDrawable = (GradientDrawable)background;
+                gradientDrawable.setColor(color);
+            }
+        }
+        return  boolSelected;
+    }
+}
