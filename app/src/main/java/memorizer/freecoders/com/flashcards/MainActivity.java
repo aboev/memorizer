070b27c8b36c1947fe5d39cfe47337e19c30e9e4
Aleksey Boev <boev.aleksey@gmail.com>
2016-02-05 23:36:04 +0300

    - Added latest version check on start

--- app/src/main/java/memorizer/freecoders/com/flashcards/MainActivity.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/MainActivity.java
@@ -143,6 +143,8 @@ public class MainActivity extends AppCompatActivity {
             networkRequests();
         }
 
+        Utils.checkLatestVersion();
+
     }
 
     public void restoreApp () {
