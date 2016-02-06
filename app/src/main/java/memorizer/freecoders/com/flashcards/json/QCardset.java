bb54bd589dcec8bf781fc0d255416eb4754a9af3
Aleksey Boev <boev.aleksey@gmail.com>
2016-02-06 12:56:08 +0300

    - Qcardset initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/QCardset.java
@@ -0,0 +1,12 @@
+package memorizer.freecoders.com.flashcards.json;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alex-mac on 06.02.16.
+ */
+public class QCardset {
+    public Integer cardset_id;
+    public String title;
+    public ArrayList<Integer> flags;
+}
