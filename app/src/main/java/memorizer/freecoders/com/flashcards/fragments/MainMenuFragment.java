aff6b49fa2f5f19956f726710d13e87ba1743a47
Aleksey Boev <boev.aleksey@gmail.com>
2016-02-13 14:03:45 +0300

    - Moved gamplay logic to Multicards.onPickCardsetCallback

--- app/src/main/java/memorizer/freecoders/com/flashcards/fragments/MainMenuFragment.java
+++ app/src/main/java/memorizer/freecoders/com/flashcards/fragments/MainMenuFragment.java
@@ -1,6 +1,7 @@
 package memorizer.freecoders.com.flashcards.fragments;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Response;
 
@@ -19,6 +21,8 @@ import memorizer.freecoders.com.flashcards.classes.CallbackInterface;
 import memorizer.freecoders.com.flashcards.common.Constants;
 import memorizer.freecoders.com.flashcards.common.InputDialogInterface;
 import memorizer.freecoders.com.flashcards.common.Multicards;
+import memorizer.freecoders.com.flashcards.dao.Cardset;
+import memorizer.freecoders.com.flashcards.json.CardSet;
 import memorizer.freecoders.com.flashcards.json.Game;
 import memorizer.freecoders.com.flashcards.json.ServerResponse;
 import memorizer.freecoders.com.flashcards.json.UserDetails;
@@ -48,10 +52,15 @@ public class MainMenuFragment extends Fragment {
         textViewTrain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Multicards.onPickCardsetCallback = new CallbackInterface() {
+                    @Override
+                    public void onResponse(Object obj) {
+                        String strGID = (String) obj;
+                        startSinglePlayer(strGID);
+                    }
+                };
                 Intent intent = new Intent(Multicards.getMainActivity(),
                         CardsetPickerActivity.class);
-                intent.putExtra(Constants.INTENT_META_NEXT_FRAGMENT,
-                        Constants.UI_STATE_TRAIN_MODE);
                 startActivity(intent);
             }
         });
@@ -59,45 +68,43 @@ public class MainMenuFragment extends Fragment {
         textViewMultiplayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                InputDialogInterface.showPickOpponentDialog(new CallbackInterface() {
-                    @Override
-                    public void onResponse(Object obj) {
-                        Integer index = (Integer) obj;
-                        if (index == 0) {
-                            GameplayManager.strOpponentName = null;
-                            Intent intent = new Intent(Multicards.getMainActivity(),
-                                    CardsetPickerActivity.class);
-                            intent.putExtra(Constants.INTENT_META_NEXT_FRAGMENT,
-                                    Constants.UI_STATE_MULTIPLAYER_MODE);
-                            startActivity(intent);
-                        } else if (index == 1) {
-                            GameplayManager.strOpponentName = "-1";
-                            Intent intent = new Intent(Multicards.getMainActivity(),
-                                    CardsetPickerActivity.class);
-                            intent.putExtra(Constants.INTENT_META_NEXT_FRAGMENT,
-                                    Constants.UI_STATE_MULTIPLAYER_MODE);
-                            startActivity(intent);
-                        } else if (index == 2) {
-                            InputDialogInterface.showEnterOpponentNameDialog(new CallbackInterface() {
-                                @Override
-                                public void onResponse(Object obj) {
-                                if (obj != null) {
-                                    String strOpponentName = (String) obj;
-                                    GameplayManager.strOpponentName = strOpponentName;
-                                    ServerInterface.newGameRequest("", strOpponentName,
-                                        new Response.Listener<ServerResponse<Game>>() {
-                                            @Override
-                                            public void onResponse(ServerResponse<Game> response) {
-                                                if (!response.isSuccess())
-                                                    InputDialogInterface.deliverError(response);
-                                            }
-                                        }, null);
-                                }
-                                }
-                            });
+            InputDialogInterface.showMultiplayerDialog(new CallbackInterface() {
+                @Override
+                public void onResponse(Object obj) {
+                Integer index = (Integer) obj;
+                if (index == 0) {
+                    Multicards.onPickCardsetCallback = new CallbackInterface() {
+                        @Override
+                        public void onResponse(Object obj) {
+                            String strGID = (String) obj;
+                            requestMultiplayerGame(true, strGID);
                         }
-                    }
-                });
+                    };
+
+                    Intent intent = new Intent(Multicards.getMainActivity(),
+                            CardsetPickerActivity.class);
+                    startActivity(intent);
+                } else if (index == 1) {
+                    InputDialogInterface.showEnterOpponentNameDialog(new CallbackInterface() {
+                        @Override
+                        public void onResponse(Object obj) {
+                            if (obj != null) {
+                                String strOpponentName = (String) obj;
+                                GameplayManager.strOpponentName = strOpponentName;
+                                ServerInterface.newGameRequest("", strOpponentName,
+                                    new Response.Listener<ServerResponse<Game>>() {
+                                        @Override
+                                        public void onResponse(ServerResponse<Game> response) {
+                                            if (!response.isSuccess())
+                                                InputDialogInterface.deliverError(response);
+                                        }
+                                    }, null);
+                            }
+                        }
+                    });
+                }
+                }
+            });
             }
         });
 
@@ -125,6 +132,60 @@ public class MainMenuFragment extends Fragment {
 
     private void saveRecentOpponent (String strOpponentName) {
 
+    }
+
+    private void requestMultiplayerGame(final Boolean boolNewGame, final String strGID) {
+        if (boolNewGame) {
+            InputDialogInterface.showEnterOpponentNameDialog(new CallbackInterface() {
+                @Override
+                public void onResponse(Object obj) {
+                    String strOpponentName = (obj != null) ? (String) obj : null;
+                    GameplayManager.strOpponentName = strOpponentName;
+                    GameplayManager.requestMultiplayerGameNew(boolNewGame, strOpponentName, strGID);
+                }
+            });
+            Multicards.getCardsetPickerActivity().finish();
+        } else {
+            GameplayManager.requestMultiplayerGame(strGID);
+            Multicards.getCardsetPickerActivity().finish();
+        }
+    }
+
+    private void startSinglePlayer (final String strGID) {
+        Cardset cardset = Multicards.getFlashCardsDAO().fetchCardset(strGID);
+        if (cardset == null) {
+            Multicards.getFlashCardsDAO().importFromWeb(strGID,
+                new CallbackInterface() {
+                    @Override
+                    public void onResponse(Object obj) {
+                        if (InputDialogInterface.progressDialog != null)
+                            InputDialogInterface.progressDialog.dismiss();
+                        Long setID = (Long) obj;
+                        GameplayManager.startSingleplayerGame(setID, strGID);
+                        Multicards.getFlashCardsDAO().setRecentCardset(strGID);
+                        Multicards.getCardsetPickerActivity().finish();
+                    }
+                }, new CallbackInterface() {
+                    @Override
+                    public void onResponse(Object obj) {
+                        Multicards.getMainActivity().getFragmentManager().beginTransaction().
+                                detach(FragmentManager.mainMenuFragment).commit();
+                        if (InputDialogInterface.progressDialog != null)
+                            InputDialogInterface.progressDialog.dismiss();
+                        Toast.makeText(Multicards.getMainActivity(),
+                                "Failed to fetch cardset",
+                                Toast.LENGTH_LONG).show();
+                        FragmentManager.returnToMainMenu(false);
+                    }
+                });
+            String strMessage = getResources().
+                    getString(R.string.download_cardset_dialog_message);
+            InputDialogInterface.showProgressBar(strMessage, null);
+        } else {
+            GameplayManager.startSingleplayerGame(cardset.getId(), strGID);
+            Multicards.getFlashCardsDAO().setRecentCardset(strGID);
+            Multicards.getCardsetPickerActivity().finish();
+        }
     }
 
 }
