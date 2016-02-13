a4964b2410a6a49b3247f6422ff39ad8b3acfa96
Aleksey Boev <boev.aleksey@gmail.com>
2016-02-13 14:11:59 +0300

    - InvitationDescriptor initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/InvitationDescriptor.java
@@ -0,0 +1,10 @@
+package memorizer.freecoders.com.flashcards.json;
+
+/**
+ * Created by alex-mac on 09.02.16.
+ */
+public class InvitationDescriptor {
+    public UserDetails user;
+    public Game game;
+    public QCardset cardset;
+}
