712dc20e56534027d5812a244e1271aa22438131
Aleksey Boev <boev.aleksey@gmail.com>
2016-03-06 19:59:28 +0300

    - ServerInfo initial commit

---
+++ app/src/main/java/memorizer/freecoders/com/flashcards/json/ServerInfo.java
@@ -0,0 +1,15 @@
+package memorizer.freecoders.com.flashcards.json;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by alex-mac on 28.11.15.
+ */
+public class ServerInfo {
+    public int version;
+    public int min_client;
+    public int latest_apk;
+    public int latest_ver;
+    public HashMap<String, String> update_comment;
+ }
